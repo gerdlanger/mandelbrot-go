fda3d83c99be13a45e9c9114b81ddc5dba58794d
Gerd Langer <gerd.langer@gmx.de>
2020-04-13 14:08:40 +0200

    zips added to build

--- build.sh
+++ build.sh
@@ -9,3 +9,6 @@ GOOS=windows GOARCH=amd64 go build -o apfel.exe
 echo Linux ARM
 GOOS=linux GOARCH=arm GOARM=7 go build -o apfel_arm
 
+zip apfel_mac.zip apfel
+zip apfel_win.zip apfel.exe
+zip apfel_arm.zip apfel_arm
