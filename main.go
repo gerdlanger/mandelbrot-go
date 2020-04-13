286a2d3228a0456703874963e09a31cfa072e81d
Gerd Langer <gerd.langer@gmx.de>
2020-04-13 15:53:54 +0200

    a bit more, modified command line args

--- main.go
+++ main.go
@@ -41,6 +41,13 @@ func main() {
 		ImgRect{-2.2, 0.8, -1.3, 1.3},
 		ImgRect{0.435396403, 0.451687191, 0.367981352, 0.380210061},
 		ImgRect{-0.37465401, -0.37332411, +0.659227668, +0.66020767},
+		ImgRect{-1.816, -1.66, -0.078, 0.078},
+		ImgRect{-0.098289376564, -0.098289374197, -0.889045279124, -0.88904527676},
+		ImgRect{-0.098289375468734, -0.098289375467142, -0.889045277967886, -0.889045277966296},
+		ImgRect{-0.867, -0.7, -0.234, 0},
+		ImgRect{-0.8085, -0.78159, -0.19071, -0.1638},
+		ImgRect{-0.80715, -0.7941, -0.18546, -0.17255},
+		ImgRect{-0.8024335, -0.802384, -0.1776984, -0.1776492},
 	}
 	// Standard
 	reMin, reMax := -2.2, 0.8
@@ -52,17 +59,16 @@ func main() {
 
 	args := os.Args[1:]
 	argc := len(args)
-	fmt.Println("argc=", argc, " args=", args)
 	for idx := 0; idx < argc; idx++ {
 		switch arg := args[idx]; arg {
 		case "h", "?":
 			fmt.Println("args:")
-			fmt.Println("c <num> => limit number of concurrent routines")
-			fmt.Println("p <num> => for NumProcs")
-			fmt.Printf("i <num> => max iteration (def=%d)\n", maxIter)
-			fmt.Println("s or xs => small or extra small image")
-			fmt.Printf("r <num> => select alternative image ranges (0-%d)\n", len(examples))
-		case "c":
+			fmt.Println("-c <num> => limit number of concurrent routines")
+			fmt.Println("-p <num> => for NumProcs")
+			fmt.Printf("-i <num> => max iteration (def=%d)\n", maxIter)
+			fmt.Println("-s <size> => <size> is one of: (s) small, (xs) extra small, (tiny), (vga) =640x480, (hdmi)=1920x1080 ")
+			fmt.Printf("-r <num> => select alternative image ranges (0-%d)\n", len(examples))
+		case "-c":
 			idx++
 			num, err := extractArg(args[idx])
 			if err == nil {
@@ -88,21 +94,21 @@ func main() {
 				}
 				fmt.Printf("c(%d) => x*y=%d*%d\n", num, goXs, goYs)
 			}
-		case "p":
+		case "-p":
 			idx++
 			num, err := extractArg(args[idx])
 			if err == nil {
 				runtime.GOMAXPROCS(num)
 				fmt.Printf("p(%d) => maxprocs=%d\n", num, num)
 			}
-		case "i":
+		case "-i":
 			idx++
 			num, err := extractArg(args[idx])
 			if err == nil {
 				maxIter = num
 				fmt.Printf("i(%d)\n", num)
 			}
-		case "r":
+		case "-r":
 			idx++
 			num, err := strconv.Atoi(args[idx])
 			if err == nil && num >= 0 && num < len(examples) {
@@ -112,14 +118,32 @@ func main() {
 			} else {
 				fmt.Println("r out of range:", num)
 			}
-		case "s":
-			width /= 2
-			height /= 2
-			fmt.Println("small pic")
-		case "xs":
-			width /= 4
-			height /= 4
-			fmt.Println("extra small pic")
+		case "-s":
+			idx++
+			switch size := args[idx]; size {
+			case "s":
+				width /= 2
+				height /= 2
+				fmt.Println("small pic")
+			case "xs":
+				width /= 4
+				height /= 4
+				fmt.Println("extra small pic")
+			case "vga":
+				width = 640
+				height = 480
+				fmt.Println("vga pic")
+			case "hdmi":
+				width = 1920
+				height = 1080
+				fmt.Println("hdmi pic")
+			case "tiny":
+				width = 320
+				height = 200
+				fmt.Println("tiny pic")
+			default:
+				fmt.Println("unknown size", size)
+			}
 		default:
 			fmt.Println("unknown:", args, ". Use h for help")
 		}
@@ -153,13 +177,17 @@ func main() {
 		select {
 		case pix := <-pixels:
 			val := pix.Iter
-			c := color.NRGBA{
-				R: uint8((val) & 255),
-				G: uint8((val) << 1 & 255),
-				B: uint8((val) << 2 & 255),
-				A: 255,
+			if val == maxIter {
+				img.Set(pix.X, pix.Y, color.Black)
+			} else {
+				c := color.NRGBA{
+					R: uint8((val) & 255),
+					G: uint8((val) << 1 & 255),
+					B: uint8((val) << 2 & 255),
+					A: 255,
+				}
+				img.Set(pix.X, pix.Y, c)
 			}
-			img.Set(pix.X, pix.Y, c)
 		case <-ready:
 			running--
 		}
@@ -255,7 +283,7 @@ func Julia(x, y, xadd, yadd, radius2 float64, maxIter int) int {
 		len2 = xx + yy
 	}
 
-	return remain
+	return maxIter - remain
 }
 
 // Apfel2 caluclates an AppleMan
